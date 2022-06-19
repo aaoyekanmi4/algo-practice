e80b328d6269e6a849f886065ee711b1c50c7cfb
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-06-19 13:08:01 -0500

    added sort to containsDuplicates

--- javascript-solutions/ContainsDuplicates.js
+++ javascript-solutions/ContainsDuplicates.js
@@ -4,5 +4,14 @@ const main = () => {
         return arr.length > numSet.size ? true : false
     }
     console.log(containsDuplicatesSet([1, 1, 4, 3, 2]))
+
+    const containsDuplicatesSort = (arr) => {
+        const sortedArray = arr.sort((a, b) => a - b)
+        for (let i = 0; i < sortedArray.length - 1; i++) {
+            if (sortedArray[i] === sortedArray[i + 1]) return true
+        }
+        return false
+    }
+    console.log(containsDuplicatesSort([1, 4, 3, 2, 1]))
 }
 main()
