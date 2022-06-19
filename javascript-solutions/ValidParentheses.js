d6ee62319edfb60de32d6aaa28525223be085bcc
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-06-19 11:17:39 -0500

    added valid parentheses problem

---
+++ javascript-solutions/ValidParentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+    const stack = []
+    for (let i = 0; i < s.length; i++) {
+        let character = s[i]
+        if (map[character]) {
+            stack.push(character)
+        }
+        else {
+            if (!stack.length) {
+                return false
+            }
+            let open = stack.pop()
+            if (map[open] !== character) {
+                return false
+            }
+        }
+    }
+    return stack.length ? false : true
+}
