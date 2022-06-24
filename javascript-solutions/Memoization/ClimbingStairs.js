a26bfe22182e37fb0913bbe9db6f8382430b6753
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-06-23 21:52:32 -0500

    added number of Islands and modified climbing stairs

--- javascript-solutions/Memoization/ClimbingStairs.js
+++ javascript-solutions/Memoization/ClimbingStairs.js
@@ -11,7 +11,19 @@ const main = () => {
         memo.set(n, steps)
         return steps
     }
-    console.log(climbingStairs(3))
+
+    const climbingStairsDp = (n) => {
+        const dp = []
+
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for (let i = 3; i <= n; i++) {
+            dp[i] = dp[i -1] + dp [i -2]
+        }
+        return dp[n]
+    }
+    console.log(climbingStairsDp(6))
 }
 
 main()
