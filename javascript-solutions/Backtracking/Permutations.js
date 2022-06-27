fe2baf645a9e2ac4d34b03c51dfbd182517f4072
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-06-27 11:43:32 -0500

    replaced permutations with another solution

--- javascript-solutions/Backtracking/Permutations.js
+++ javascript-solutions/Backtracking/Permutations.js
@@ -1,20 +1,23 @@
-const permute = (nums) => {
-    const result = []
-    let firstElement
-    if (nums.length === 1) {
-        numsCopy = [...nums]
-        return [numsCopy]
-    }
-    for (let i = 0; i < nums.length; i++) {
-        firstElement = nums.shift()
-        let perms = permute(nums)
-        console.log(perms)
-        for (let perm of perms) {
-            perm.push(firstElement)
+const main = () => {
+    //The idea is to break down the input into it's smallest pieces, arrange them and then bubble
+    //back up using recursion
+    function permute (nums) {
+        const result = []
+        //base case
+        if (nums.length === 1) return [nums.slice()]
+
+        for (let num of nums) {
+            let n = nums.shift()
+            const perms = permute(nums)
+            for (let perm of perms) {
+                perm.push(n)
+            }
+            perms.forEach((perm) => result.push(perm))
+            nums.push(n)
         }
-        result.push(perms)
-        nums.push(firstElement)
+
+        return result
     }
-    return result.flat()
+    console.log(permute([1, 2, 3]))
 }
-console.log(permute([1, 2, 3]))
+main()
